78a76064bb65b90fe5e7e0251fd57e18e7a2aefb
Milena <mcabrerapatino@gmail.com>
2020-11-19 05:44:11 -0500

    feat(ui): adding boilerplate

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Reputation from "./components/Reputation/Reputation.tsx"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Reputation className="App-Reputation">
+        
+      </Reputation>
+    </div>
+  );
+}
+
+export default App;
